777bcb0080eb9336e8418baba504303b6bb7bbf8
Franck Nijhof <frenck@geekchimp.com>
2017-12-13 00:07:20 +0100

    :arrow_up: Synced libraries and files with the current Alpine base

---
+++ debian-base/rootfs/etc/cont-finish.d/99-message.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ==============================================================================
+# Community Hass.io Add-ons: Base Images
+# Displays an message right before terminating in case something went wrong
+# ==============================================================================
+
+if [[ "${S6_STAGE2_EXITED}" -ne 0 ]]; then
+    echo '-----------------------------------------------------------'
+    echo '                Oops! Something went wrong.'
+    echo ' '
+    echo ' We are so sorry, but something went terribly wrong when'
+    echo ' starting or running this add-on.'
+    echo ' '
+    echo ' Be sure to check the log above, line by line, for hints.'
+    echo '-----------------------------------------------------------'
+fi
