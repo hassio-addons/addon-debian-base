f8e25cb1f3632f32704bc6103c105311a21105f3
Franck Nijhof <git@frenck.dev>
2022-06-09 10:46:25 +0200

    Fix compatibility with s6-overlay 3x

--- base/rootfs/etc/cont-init.d/02-set-timezone.sh
+++ base/rootfs/etc/cont-init.d/02-set-timezone.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Base Images
 # Configures the timezone
 # ==============================================================================
-
 if ! bashio::var.is_empty "${TZ}"; then
     bashio::log.info "Configuring timezone"
 
