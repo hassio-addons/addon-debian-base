5c12f8b26a1a9ebd4be8afece3e0a54cb970b5e7
Felipe Santos <felipecassiors@gmail.com>
2022-11-14 14:10:17 -0300

    Fix unbound TZ variable
    This happens when the TZ environment variable is not set, like when running outside of Home Assistant.

--- base/rootfs/etc/cont-init.d/02-set-timezone.sh
+++ base/rootfs/etc/cont-init.d/02-set-timezone.sh
@@ -3,7 +3,7 @@
 # Home Assistant Community Add-on: Base Images
 # Configures the timezone
 # ==============================================================================
-if ! bashio::var.is_empty "${TZ}"; then
+if ! bashio::var.is_empty "${TZ:-}"; then
     bashio::log.info "Configuring timezone"
 
     ln --symbolic --no-dereference --force "/usr/share/zoneinfo/${TZ}" /etc/localtime
