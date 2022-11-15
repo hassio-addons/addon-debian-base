490617f5474f742c5bc5b0aab5fc5d3d43a66d5c
Felipe Santos <felipecassiors@gmail.com>
2022-11-15 14:09:40 -0300

    Fix unbound TZ variable (#127)

--- base/rootfs/etc/cont-init.d/02-set-timezone.sh
+++ base/rootfs/etc/cont-init.d/02-set-timezone.sh
@@ -3,9 +3,8 @@
 # Home Assistant Community Add-on: Base Images
 # Configures the timezone
 # ==============================================================================
-if ! bashio::var.is_empty "${TZ}"; then
-    bashio::log.info "Configuring timezone"
 
-    ln --symbolic --no-dereference --force "/usr/share/zoneinfo/${TZ}" /etc/localtime
-    echo "${TZ}" > /etc/timezone
-fi
+bashio::log.info "Configuring timezone"
+
+ln --symbolic --no-dereference --force "/usr/share/zoneinfo/${TZ}" /etc/localtime
+echo "${TZ}" > /etc/timezone
